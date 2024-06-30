859b77b6a74accce5ad06222a4948560c5d8ba76
bo <q463250938@gmail.com>
2024-06-30 15:07:13 -0700

    Initial Commit

---
+++ ActorsBootcamp/ActorsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorsBootcampApp.swift
+//  ActorsBootcamp
+//
+//  Created by Bo Zhong on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ActorsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
