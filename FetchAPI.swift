ab79748904761d4b667d7de5ce2acc0d85457724
Bob <q463250938@gmail.com>
2024-07-10 13:31:33 -0700

    [Feature]  added Change heart icon

--- FetchAPI.swift
+++ FetchAPI.swift
@@ -5,4 +5,36 @@
 //  Created by Bo Zhong on 7/10/24.
 //
 
+struct Beer: Codable, Identifiable {
+    let title: String
+    let description: String
+    let ingredients: [String]
+    let image: String
+    let id: Int
+}
+
 import Foundation
+
+func fetchBeer() {
+    guard let url = URL(string: "https://api.sampleapis.com/coffee/hot") else { return }
+    
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        guard let data = data,
+              let response = response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            return
+        }
+        
+        do {
+            let decodedData = try JSONDecoder().decode([Beer].self, from: data)
+            print(decodedData)
+        } catch {
+            fatalError()
+        }
+    }
+    
+    task.resume()
+}
+
+
+
