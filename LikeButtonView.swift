2a740674de0a8d03bf66bf693f37099b276a708c
Bob <q463250938@gmail.com>
2024-07-10 13:33:00 -0700

    [Feature] Changed heart icon to ABC Heart

--- LikeButtonView.swift
+++ LikeButtonView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LikeButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("ABC_Heart_ICON")
     }
 }
 
